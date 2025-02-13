4c2b69ae9b7c2057b57e16ee1647f837ceb96832
Willie Shen <cg10250207@gmail.com>
2025-02-13 23:49:26 +0800

    1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.willie.mancala"
         minSdk = 21
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
         
         vectorDrawables {
             useSupportLibrary = true
